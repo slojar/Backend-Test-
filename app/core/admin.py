c3f375743da449bac09659b4099ec5377ec43c9d
slojararshavin <sunday@tm30.net>
2024-07-18 23:35:24 +0100

    FIX[ADDED MODELS IN ADMIN]

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,20 @@
-"""Manage admin page for main app."""
+from django.contrib import admin
+from core.models import User, ProductCategory, Product, Order, OrderDetail
 
-# from django.contrib import admin
 
-# Register your models here.
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "created_on", "updated_on"]
+    list_filter = ["price", "created_on"]
+    search_fields = ["name"]
+
+
+class OrderModelAdmin(admin.ModelAdmin):
+    list_display = ["user", "created_on", "updated_on"]
+
+
+admin.site.register(User)
+admin.site.register(ProductCategory)
+admin.site.register(OrderDetail)
+admin.site.register(Product, ProductModelAdmin)
+admin.site.register(Order, OrderModelAdmin)
+
